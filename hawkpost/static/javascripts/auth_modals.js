e66bfe94a75a04c251f4c32ca29a18e0c879bcfb
aservices <arunkumar.g@cogzidel.com>
2018-01-25 10:45:09 +0530

    removed whole package

--- hawkpost/static/javascripts/auth_modals.js
+++
@@ -1,5 +0,0 @@
-$("#id_email").addClass("text padding-modals1");
-$("#id_password1").addClass("text padding-modals1");
-$("#id_password2").addClass("text padding-modals1");
-$("#id_login").addClass("text padding-modals1");
-$("#id_password").addClass("text padding-modals1");
--- pages/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from .views import HomeView, AboutView, HelpView
-
-urlpatterns = [
-    url(r'^$', HomeView.as_view(), name="pages_index"),
-    url(r'^about$', AboutView.as_view(), name="pages_about"),
-    url(r'^help$', HelpView.as_view(), name="pages_help")
-]
